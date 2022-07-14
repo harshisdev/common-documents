e3b7e5b277a423a40335c04f4c32fda3ec0831c4
Harsh Kumar <harshch9931@gmail.com>
2022-07-14 13:54:34 +0530

    commercial
    commercial website only frontend design no backend use

---
+++ assets/js/main.js
@@ -0,0 +1,62 @@
+// loder
+var lodingpage = document.getElementById("loding");
+const myTimeout = setTimeout(load(), 5000);
+function load() {
+  lodingpage.style.display = "none";
+}
+
+// nav bar sticky
+document.addEventListener("DOMContentLoaded", function () {
+  window.addEventListener("scroll", function () {
+    if (window.scrollY > 82) {
+      document.getElementById("navbar_top").classList.add("fixed-top");
+      // add padding top to show content behind navbar
+      navbar_height = document.querySelector(".navbar").offsetHeight;
+      document.body.style.paddingTop = navbar_height + "px";
+    } else {
+      document.getElementById("navbar_top").classList.remove("fixed-top");
+      // remove padding top from body
+      document.body.style.paddingTop = "0";
+    }
+  });
+});
+
+//Get the button
+var mybutton = document.getElementById("top");
+
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function () {
+  scrollFunction();
+};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 10) {
+    mybutton.style.display = "flex";
+  } else {
+    mybutton.style.display = "none";
+  }
+}
+
+// When the user clicks on the button, scroll to the top of the document
+function topbuttom() {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
+}
+// login page
+var modal = document.getElementById("id01");
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function (event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+};
+// login
+function showPassword() {
+  var password = document.getElementById("password");
+  if (password.type === "password") {
+    password.type = "text";
+  } else {
+    password.type = "password";
+  }
+}
